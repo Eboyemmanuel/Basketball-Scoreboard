10387368ebaae087fe48c7da52888b8d60f08520
Eboyemmanuel <eboyemmanuel@gmail.com>
2023-01-27 06:22:28 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+let count = 0
+let count1 = 0
+let Score = document.getElementById("scores")
+let Scores = document.getElementById("scoress")
+//console.log(Score)
+
+function increment1() {
+    count += 1
+    Score.textContent = count
+}
+
+function increment2() {
+    count += 2
+    Score.textContent = count
+}
+
+function increment3() {
+    count += 3
+    Score.textContent = count
+}
+
+function increment11() {
+    count1 += 1
+    Scores.textContent = count1
+}
+
+function increment22() {
+    count1 += 2
+    Scores.textContent = count1
+}
+
+function increment33() {
+    count1 += 3
+    Scores.textContent = count1
+}
